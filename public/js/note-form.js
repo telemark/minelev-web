709619bae941234c3cb757e6340d1fe48490a8f7
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-05-14 13:17:33 +0200

    Adds text field and validation (patch)

--- public/js/note-form.js
+++ public/js/note-form.js
@@ -2,14 +2,20 @@
 
 function init () {
   var radios = document.querySelectorAll('.note-type-selector')
+  var noteContentField = document.getElementById('noteContent')
 
   validateDocumentForm()
+
   Array.prototype.forEach.call(radios, function(el) {
     el.addEventListener('click', function (e) {
       validateDocumentForm()
     })
   })
   
+  noteContentField.addEventListener('keyup', function (e) {
+    validateDocumentForm()
+  })
+
   initPreview()
 }
 
@@ -17,6 +23,7 @@ function validateDocumentForm () {
   var submitButton = document.getElementById('submitNote')
   var previewButton = document.getElementById('previewDocumentButton')
   var noteTypes = document.querySelectorAll('.note-type-selector')
+  var noteContentField = document.getElementById('noteContent')
   var type = false
   var typeOK = false
   // Starts by disabling button
@@ -31,7 +38,7 @@ function validateDocumentForm () {
   })
 
   // If everything is OK let's go :-)
-  if (typeOK) {
+  if (typeOK && noteContentField.value.length > 0) {
     submitButton.disabled = false
     previewButton.disabled = false
   }
