f8a510e45bac50e5ec2ffa121aa2c6a7872d6cf7
maccyber <jonas.enge@gmail.com>
2019-10-09 16:13:24 +0200

    Revert: Adds tjommi token

--- config/index.js
+++ config/index.js
@@ -7,7 +7,6 @@ module.exports = {
   COOKIE_SECRET: process.env.COOKIE_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   ENCRYPTOR_SECRET: process.env.ENCRYPTOR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
-  TJOMMI_JWT_SECRET: process.env.TJOMMI_JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   YAR_SECRET: process.env.YAR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL || 'https://feide.tjeneste.win',
   BUDDY_SERVICE_URL: process.env.BUDDY_SERVICE_URL || 'https://buddy.minelev.win',
